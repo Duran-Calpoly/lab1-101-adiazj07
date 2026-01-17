ad300cc8493c6f1d21bca1499c703b04e61436e1
adiazj07 <adiaz187@calpoly.edu>
2026-01-17 11:11:37 +0000

    Lab 1 Done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,10 @@
-pass
+def calculate_average(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+
+def add_tax(bill_total):
+    return (bill_total / 10) + bill_total
+
+def greet_user(name):
+    return f"Hello {name}"
+
+
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,22 @@
-pass
+def check_multiple(number: int):
+    if number % 3 == 0 and number % 5 == 0:
+        return True
+    else:
+        return False
+
+def check_password(input_string: str):
+    if input_string == "Python123":
+        return "access granted"
+    else:
+        return "access denied"
+
+def calculate_federal_tax(salary: int):
+    if salary > 95375:
+        return (salary / 100) * 24
+    elif salary > 44725:
+        return (salary / 100) * 22
+    elif salary > 11000:
+        return (salary / 100) * 12
+    else:
+        return (salary / 100) * 10
+    
